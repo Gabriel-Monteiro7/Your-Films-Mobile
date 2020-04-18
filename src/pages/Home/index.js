1b9b92441518ad88025f94fb0e75de065057b0d8
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-04-18 12:42:51 -0300

    Adiçao do carousel para os cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,10 @@
 import React from "react";
-
+import { FlatList, View } from "react-native";
 import { Container, Title, Header, Icon, ContainerIcon } from "./styles";
 import Card from "../../components/Card";
+import Carousel from "react-native-snap-carousel";
+import { width } from "../../util";
+
 export default function Home() {
   return (
     <Container>
@@ -11,7 +14,18 @@ export default function Home() {
           <Icon name={"search"} />
         </ContainerIcon>
       </Header>
-      <Card />
+      <View style={{ flex: 1, justifyContent: "flex-start" }}>
+        <Carousel
+          // ref={(c) => {
+          //   this.carousel = c;
+          // }}
+          data={[1, 2]}
+          renderItem={({ item, index, separators }) => <Card />}
+          sliderWidth={500}
+          itemWidth={300}
+          containerCustomStyle={{ marginLeft: -70 }}
+/>
+      </View>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -3,7 +3,6 @@ import { hp, wp, height as heightValue } from "../../util";
 import Constants from "expo-constants";
 import { Feather } from "@expo/vector-icons";
 export const Container = styled.View`
-  padding: 0px 30px;
   background: #111111;
   flex: 1;
   padding-top: ${Constants.statusBarHeight + hp(5)}px;
@@ -12,6 +11,7 @@ export const Header = styled.View`
   flex-direction: row;
   align-items: baseline;
   justify-content: space-between;
+  padding: 0px 30px;
 `;
 export const Title = styled.Text`
   color: white;
