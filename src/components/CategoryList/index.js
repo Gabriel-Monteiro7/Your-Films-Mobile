98675cb8eab171ef18b80f4094c4110b823b82f3
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-05-03 21:39:43 -0300

    Mudanças nos cards e na lista de categorias

--- src/components/CategoryList/index.js
+++ src/components/CategoryList/index.js
@@ -1,30 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FlatList } from "react-native";
 
 import { Container, CategoryTitle, Line, ContainerCategory } from "./styles";
+import { valueSelected } from "../../services/api";
 
 export default function CategoryList() {
+  const [categorias, setCategorias] = useState([]);
+
   const [selected, setSelected] = useState("0");
+  useEffect(() => {
+    setCategorias(valueSelected.option.categories);
+  }, []);
   return (
     <Container>
       <FlatList
         horizontal={true}
-        data={[
-          { id: "0", title: "All" },
-          { id: "1", title: "Terror" },
-          { id: "2", title: "Ação" },
-          { id: "3", title: "Suspense" },
-        ]}
+        data={categorias}
         renderItem={({ item }) => (
           <ContainerCategory
             onTouchEnd={() => {
               setSelected(item.id);
             }}
           >
-            <CategoryTitle active={item.id === selected}>
+            <CategoryTitle active={item.id === valueSelected.category}>
               {item.title}
             </CategoryTitle>
-            <Line active={item.id === selected} />
+            <Line active={item.id === valueSelected.category} />
           </ContainerCategory>
         )}
         keyExtractor={(item) => item.id}
